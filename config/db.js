0f862abfe796cbdf674a572fb225553b3b3b86f7
Yatharth Vats <vatsyatharth2000@gmail.com>
2020-05-15 22:13:20 +0530

    Added basic routes

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+//create a default.json file in config folder
+//and add in your mongoDB project connection there under mongoURI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log(err.message);
+    //Exiting the process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
